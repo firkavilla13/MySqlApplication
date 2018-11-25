5b74e4e8a275358c566e5638fe0328d03b1c18b3
Gabor <Gabor@DESKTOP-0RI5UCJ>
2018-11-25 11:31:08 +0100

    Kész

--- MysqlApllciation/Form1.Designer.cs
+++ MysqlApllciation/Form1.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.nevTextBox = new System.Windows.Forms.TextBox();
+            this.jelszoTextBox = new System.Windows.Forms.TextBox();
+            this.szuletesDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.regButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nevTextBox
+            // 
+            this.nevTextBox.Location = new System.Drawing.Point(27, 27);
+            this.nevTextBox.Name = "nevTextBox";
+            this.nevTextBox.Size = new System.Drawing.Size(209, 20);
+            this.nevTextBox.TabIndex = 0;
+            // 
+            // jelszoTextBox
+            // 
+            this.jelszoTextBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.jelszoTextBox.Location = new System.Drawing.Point(27, 54);
+            this.jelszoTextBox.Name = "jelszoTextBox";
+            this.jelszoTextBox.PasswordChar = '*';
+            this.jelszoTextBox.Size = new System.Drawing.Size(209, 20);
+            this.jelszoTextBox.TabIndex = 1;
+            // 
+            // szuletesDateTimePicker
+            // 
+            this.szuletesDateTimePicker.Location = new System.Drawing.Point(27, 80);
+            this.szuletesDateTimePicker.Name = "szuletesDateTimePicker";
+            this.szuletesDateTimePicker.Size = new System.Drawing.Size(209, 20);
+            this.szuletesDateTimePicker.TabIndex = 2;
+            // 
+            // regButton
+            // 
+            this.regButton.Location = new System.Drawing.Point(27, 116);
+            this.regButton.Name = "regButton";
+            this.regButton.Size = new System.Drawing.Size(75, 23);
+            this.regButton.TabIndex = 3;
+            this.regButton.Text = "Regisztracio";
+            this.regButton.UseVisualStyleBackColor = true;
+            this.regButton.Click += new System.EventHandler(this.regButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(27, 158);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Törlés";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(27, 205);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 5;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.regButton);
+            this.Controls.Add(this.szuletesDateTimePicker);
+            this.Controls.Add(this.jelszoTextBox);
+            this.Controls.Add(this.nevTextBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox nevTextBox;
+        private System.Windows.Forms.TextBox jelszoTextBox;
+        private System.Windows.Forms.DateTimePicker szuletesDateTimePicker;
+        private System.Windows.Forms.Button regButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
     }
 }
 
--- MysqlApllciation/Form1.cs
+++ MysqlApllciation/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace MysqlApllciation
 {
@@ -16,5 +17,85 @@ namespace MysqlApllciation
         {
             InitializeComponent();
         }
+
+        private void regButton_Click(object sender, EventArgs e)
+        {
+            string nev = nevTextBox.Text;
+            string jelszo = jelszoTextBox.Text;
+            DateTime regdatum = szuletesDateTimePicker.Value;
+
+            using (var conn = new MySqlConnection(
+                "Server=localhost;Database=regisztracio;Uid=root;Password=;"))
+            {
+                conn.Open();
+                var ellenorzes = conn.CreateCommand();
+                ellenorzes.CommandText = "SELECT COUNT(*) FROM felhasznalo WHERE nev = @nev";
+                ellenorzes.Parameters.AddWithValue(@"nev", nev);
+                var darab = (long)ellenorzes.ExecuteScalar();
+                if (darab!=0)
+                {
+                    MessageBox.Show("A user mar letezik");
+                    return;
+                }
+
+                var command = conn.CreateCommand();
+                command.CommandText = "INSERT INTO felhasznalo (nev, jelszo, regdatum) VALUES (@nev, @jelszo, @regdatum)";
+                command.Parameters.AddWithValue("@nev", nev);
+                command.Parameters.AddWithValue("@jelszo", jelszo);
+                command.Parameters.AddWithValue("@regdatum",regdatum);
+                int erintettSorok = command.ExecuteNonQuery();
+
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            string nev = nevTextBox.Text;
+            using (var conn = new MySqlConnection(
+                "Server=localhost;Database=regisztracio;Uid=root;Password=;"))
+            {
+                conn.Open();                
+                var command = conn.CreateCommand();
+                command.CommandText = "DELETE FROM felhasznalo WHERE nev = @nev";
+                command.Parameters.AddWithValue("@nev", nev);                         
+                int erintettSorok = command.ExecuteNonQuery();
+                if (command.ExecuteNonQuery()==0)
+                {
+                    MessageBox.Show("Felhasználó törölve!");
+                }
+                else
+                {
+                    MessageBox.Show("Sikertelen törlés!");
+
+                }
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            string nev = nevTextBox.Text;
+            DateTime regdatum = szuletesDateTimePicker.Value;
+
+            using (var conn = new MySqlConnection(
+                "Server=localhost;Database=regisztracio;Uid=root;Password=;"))
+            {
+                conn.Open();
+                var command = conn.CreateCommand();
+                command.CommandText = "UPDATE  felhasznalo SET regdatum = @regdatum WHERE nev = @nev";
+                command.Parameters.AddWithValue("@nev", nev);
+                command.Parameters.AddWithValue("@regdatum", regdatum);
+                int erintettSorok = command.ExecuteNonQuery();
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Felhasználó szüledési dátum módósítva!");
+                }
+                else
+                {
+                    MessageBox.Show("Sikertelen módosítás!");
+
+                }
+            }
+        }
     }
+    
 }
